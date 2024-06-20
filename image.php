3e6a8326912d4cdf636892f5a1ec07c54ff05a62
chems <chayma.kilic@lepoles.org>
2024-06-20 17:05:28 +0200

    20062024

--- image.php
+++ image.php
@@ -1,3 +1,14 @@
 <?php include __DIR__ . "/public/header.html.php"; ?>
-<main>image</main>
+<main>
+    <section>
+        <div>
+            <h1>Image</h1>
+        </div>
+        <div class="content_img">
+            <h2> Image de fond en CSS</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta pariatur, consectetur doloribus quidem reprehenderit iste omnis excepturi inventore ad earum temporibus sapiente accusamus. Eaque minus alias vitae a impedit, itaque ut voluptatum, aperiam voluptate fugiat recusandae officia numquam quia rerum illum, aspernatur harum amet dolorum iure! Natus illo inventore dignissimos.</p>
+        
+        </div>
+    </section>
+</main>
 <?php include __DIR__ . "/public/footer.html.php"; ?>
--- tableau.php
+++ tableau.php
@@ -1,3 +1,44 @@
 <?php include __DIR__ . "/public/header.html.php"; ?>
-<main>tableau</main>
-<?php include __DIR__ . "/public/footer.html.php"; ?>
+<main>
+    <table class="tableau_style">
+        <h4>Vols Paris Rome</h4>
+<thead>
+    <tr>
+        <th></th>
+        <th>02/08/2017</th>
+        <th>03/08/2017</th>
+        <th>05/08/2017</th>
+    </tr>
+</thead>
+ 
+<tbody>
+  <tr>
+  <td>Air France</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+  <tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+  <tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+  <tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+</tbody>
+</main>
