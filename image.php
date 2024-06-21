15aa0189c5c3873426cc0286b938ecb691269f18
chems <chayma.kilic@lepoles.org>
2024-06-21 16:54:38 +0200

    21062024

--- image.php
+++ image.php
@@ -4,11 +4,31 @@
         <div>
             <h1>Image</h1>
         </div>
-        <div class="content_img">
+        <div class="content_img">            
             <h2> Image de fond en CSS</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta pariatur, consectetur doloribus quidem reprehenderit iste omnis excepturi inventore ad earum temporibus sapiente accusamus. Eaque minus alias vitae a impedit, itaque ut voluptatum, aperiam voluptate fugiat recusandae officia numquam quia rerum illum, aspernatur harum amet dolorum iure! Natus illo inventore dignissimos.</p>
-        
+            <div class="paragraphe_img">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta pariatur, consectetur doloribus quidem reprehenderit iste omnis excepturi inventore ad earum temporibus sapiente accusamus. Eaque minus alias vitae a impedit, itaque ut voluptatum, aperiam voluptate fugiat recusandae officia numquam quia rerum illum, aspernatur harum amet dolorum iure! Natus illo inventore dignissimos.</p>
+            </div>
         </div>
     </section>
+
+    <section class="img_TourEiffel">
+        <div id="first">
+            <img src="public\assets\img\toureiffel.jpg" alt="">
+        </div>
+    </section>
+
+    <section class="Ponts">
+        <div id="one">
+<img src="public\assets\img\image.jpg" alt="">
+        </div>
+        <div id="two">
+<img src="public\assets\img\image.jpg" alt="">
+        </div>
+        <div id="tree">
+<img src="public\assets\img\image.jpg" alt="">
+        </div>
+    </section>
+
 </main>
 <?php include __DIR__ . "/public/footer.html.php"; ?>
